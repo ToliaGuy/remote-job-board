c0948fc40ce0f3a69022ac43b475d305a0420279
ToljaFrajer <andruscenko@email.cz>
2023-01-01 17:28:26 +0100

    Add skeleton for uploading jobs into the DB

--- scrape.py
+++ scrape.py
@@ -1,7 +1,7 @@
 import os
 from celery import Celery
 from celery.schedules import crontab
-from scraper.main import run_scraper
+from scraper.main import run_scraper, upload_jobs
 
 app = Celery(__name__, broker=os.environ['BROKER_URL'])
 
@@ -13,4 +13,6 @@ def setup_periodic_tasks(sender, **kwargs):
 
 @app.task
 def scrape():
-    return run_scraper()
+    jobs = run_scraper()
+    upload_jobs(jobs)
+    return
--- scraper/main.py
+++ scraper/main.py
@@ -3,8 +3,14 @@ from remotive import scrape_remotive
 from himalayas import scrape_himalayas
 
 def run_scraper():
-    print("running scraper")
     jobs = []
     jobs.extend(scrape_remote_ok())
     jobs.extend(scrape_remotive())
     jobs.extend(scrape_himalayas())
+    return jobs
+
+def upload_jobs(jobs):
+    for job in jobs:
+        #TODO: Upload job to database
+        print(job)
+    return
