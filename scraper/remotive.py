fdff8ace6e0a3be0a81f4bac61cb35cb14bc7fd8
ToljaFrajer <andruscenko@email.cz>
2023-01-02 18:36:50 +0100

    Add exception handling

--- scraper/remotive.py
+++ scraper/remotive.py
@@ -2,15 +2,18 @@ import requests
 
 
 def scrape_remotive():
-    response = requests.get("https://remotive.com/api/remote-jobs")
-    json_data = response.json()
-    jobs = []
-    for job in json_data["jobs"]:
-        jobs.append({
-            "title": job["title"],
-            "company": job["company_name"],
-            "link": job["url"],
-            "description": job["description"],
-            "source": "remotive"
-        })
-    return jobs
+    try:
+        response = requests.get("https://remotive.com/api/remote-jobs")
+        json_data = response.json()
+        jobs = []
+        for job in json_data["jobs"]:
+            jobs.append({
+                "title": job["title"],
+                "company": job["company_name"],
+                "link": job["url"],
+                "description": job["description"],
+                "source": "remotive"
+            })
+        return jobs
+    except Exception as e:
+        print(e)
