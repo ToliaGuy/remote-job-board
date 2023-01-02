09b3a0ded39f01158345e9cf689aeb3fe50647d3
ToljaFrajer <andruscenko@email.cz>
2023-01-02 20:56:06 +0100

    Add connection to postgres from scraper

--- scraper/main.py
+++ scraper/main.py
@@ -1,4 +1,37 @@
 import requests
+import psycopg2
+
+
+def connect():
+    """ Connect to the PostgreSQL database server """
+    conn = None
+    try:
+        # connect to the PostgreSQL server
+        print('Connecting to the PostgreSQL database...')
+        conn = psycopg2.connect(host="db", database="postgres", user="postgres", password="postgres")
+		
+        # create a cursor
+        cur = conn.cursor()
+        
+	# execute a statement
+        print('PostgreSQL database version:')
+        cur.execute('SELECT version()')
+
+        # display the PostgreSQL database server version
+        db_version = cur.fetchone()
+        print(db_version)
+
+        #cur.execute("SELECT * FROM jobs_jobpost")
+        #print(cur.fetchall())
+       
+	# close the communication with the PostgreSQL
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+            print('Database connection closed.')
 
 def scrape_himalayas():
     print("scraping himalayas")
@@ -78,12 +111,13 @@ def upload_jobs(jobs, source):
     try:
         if len(jobs) != 0:
             data = {"jobs": jobs, "source": source}
-            requests.post("", json=data)
+            requests.post("djangoapp:8000/jobs/api", json=data)
     except Exception as e:
         print(e)
 
 
 def run_scraper():
+    connect()
     upload_jobs(scrape_remote_ok(), "remoteok")
     upload_jobs(scrape_remotive(), "remotive")
     upload_jobs(scrape_himalayas(), "himalayas")
