fa6825b31498c4162d724971c62b9357b5359dd6
ToljaFrajer <andruscenko@email.cz>
2023-01-02 20:07:39 +0100

    Have issues with importing own modules

--- scraper/main.py
+++ scraper/main.py
@@ -1,12 +1,78 @@
 import requests
-from remoteok import scrape_remote_ok
-from remotive import scrape_remotive
-from himalayas import scrape_himalayas
 
-def run_scraper():
-    upload_jobs(scrape_remote_ok(), "remoteok")
-    upload_jobs(scrape_remotive(), "remotive")
-    upload_jobs(scrape_himalayas(), "himalayas")
+def scrape_himalayas():
+    print("scraping himalayas")
+    return []
+    try:
+        jobs = []
+        offset = 0
+        limit = 200
+        while True:
+            response = requests.get(f"https://himalayas.app/jobs/api?limit={limit}&offset={offset}")
+            json_data = response.json()
+            raw_jobs = json_data["jobs"]
+            if len(raw_jobs) == 0:
+                break
+            else:
+                for raw_job in raw_jobs:
+                    jobs.append({
+                        "title": raw_job["title"],
+                        "company": raw_job["companyName"],
+                        "link": raw_job["applicationLink"],
+                        "description": raw_job["description"],
+                        "source": "himalayas"
+                    })
+            offset = offset + limit
+        return jobs
+    except Exception as e:
+        print(e)
+    
+
+def scrape_remote_ok():
+    print("scraping remote ok")
+    return []
+    try:
+        url = 'https://remoteok.com/api'
+        response = requests.get(url)
+        jobs_json = response.json()
+        raw_jobs = jobs_json[1:]
+        jobs = []
+        for job in raw_jobs:
+            jobs.append({
+                'title': job['position'],
+                'company': job['company'],
+                'link': job['apply_url'],
+                'description': job['description'],
+                'source': 'remoteok'
+            })
+        return jobs
+    except Exception as e:
+        print(e)
+
+
+
+
+def scrape_remotive():
+    print("scraping remotive")
+    return []
+    try:
+        response = requests.get("https://remotive.com/api/remote-jobs")
+        json_data = response.json()
+        jobs = []
+        for job in json_data["jobs"]:
+            jobs.append({
+                "title": job["title"],
+                "company": job["company_name"],
+                "link": job["url"],
+                "description": job["description"],
+                "source": "remotive"
+            })
+        return jobs
+    except Exception as e:
+        print(e)
+
+
+
 
 def upload_jobs(jobs, source):
     try:
@@ -15,3 +81,9 @@ def upload_jobs(jobs, source):
             requests.post("", json=data)
     except Exception as e:
         print(e)
+
+
+def run_scraper():
+    upload_jobs(scrape_remote_ok(), "remoteok")
+    upload_jobs(scrape_remotive(), "remotive")
+    upload_jobs(scrape_himalayas(), "himalayas")
