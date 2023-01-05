d07225299b617d4624115af069b6adcc30d0e654
ToljaFrajer <andruscenko@email.cz>
2023-01-05 19:17:09 +0100

    Change url path, to comply with urls django patterns

--- scraper/main.py
+++ scraper/main.py
@@ -80,9 +80,9 @@ def upload_data(jobs, source):
     # upload data all at once to api using requests
     try:
         # delete all jobs from source
-        del_res = requests.delete(f"http://djangoapp:8000/jobs/api/", json={"source": source})
+        del_res = requests.delete(f"http://djangoapp:8000/api/", json={"source": source})
         for job in jobs:
-            response = requests.post("http://djangoapp:8000/jobs/api/", json=job)
+            response = requests.post("http://djangoapp:8000/api/", json=job)
     except Exception as e:
         print(e)
 
