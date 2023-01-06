ae530c687bf79e94bee5838019e63393fbc05041
ToljaFrajer <andruscenko@email.cz>
2023-01-06 18:07:34 +0100

    Change time period between runs to 6 hours

--- scrape.py
+++ scrape.py
@@ -7,8 +7,8 @@ app = Celery(__name__, broker=os.environ['BROKER_URL'])
 
 @app.on_after_configure.connect
 def setup_periodic_tasks(sender, **kwargs):
-    # Calls matcher every day 
-    sender.add_periodic_task(10, scrape.s(), name='run scraper')
+    # Calls scraper every 6 hours
+    sender.add_periodic_task(21600, scrape.s(), name='run scraper')
 
 
 @app.task
